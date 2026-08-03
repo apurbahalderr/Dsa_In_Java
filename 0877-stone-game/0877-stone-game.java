d4cab39b48e9a7ba26ea5bedb75a4850a002a4d5
Apurba Halder <apurbahalder.rnc@gmail.com>
2026-08-03 10:38:51 +0530

    Time: 0 ms (100%), Space: 43.2 MB (49.04%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        //as alice is choosing first so she can compute whether odd indexed value is greater or even indexed value is greater
+        //then she can force bob to take the smaller indexed pile and can win always
+        return true;
+    }
+}
