dbc83e1e925be04b33aeca01230e934459146239
Apurba Halder <apurbahalder.rnc@gmail.com>
2026-08-23 21:27:33 +0530

    Time: 0 ms (100%), Space: 43.1 MB (70.98%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -15,14 +15,22 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> arr = new ArrayList<>();
-        func(root,arr);
-        return arr;
-    }
-    public void func(TreeNode root,List<Integer> arr){
-        if(root==null) return;
-        func(root.left,arr);
-        arr.add(root.val);
-        func(root.right,arr);
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode node = root;
+        List<Integer> inorder = new ArrayList<>();
+        while (true) {
+            if (node != null) {
+                st.push(node);
+                node = node.left;
+            } else {
+                if (st.isEmpty()) {
+                    break;
+                }
+                node = st.pop();
+                inorder.add(node.val);
+                node = node.right;
+            }
+        }
+        return inorder;
     }
 }
